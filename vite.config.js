8d69755e3bbc3a923c1263c71387b2755eb8d222
shinannkaku <shinannkaku@gmail.com>
2025-03-28 16:03:04 +0800

    0328vite小修改
    還要再抓錯誤

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Ushare_website/', // 這裡應填寫您的存儲庫名稱
+});
