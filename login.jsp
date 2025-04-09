028d7624ba7621a7b5a6f8f144321c0f594bcfbc
shinannkaku <shinannkaku@gmail.com>
2025-04-10 00:19:38 +0800

    0410 處理好新的html 兩頁
    #新想法：index.html進登入後可縮放左側欄位
    登入後可看到index.html&index2.html

--- login.jsp
+++ login.jsp
@@ -14,11 +14,12 @@
 <body>
     <h2>使用 LINE 登入</h2>
     <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code
-    &client_id=<%= clientId %>
-    &redirect_uri=<%= redirectUri %>
-    &state=<%= state %>
+    &client_id=2007150725
+    &redirect_uri=http://localhost:8080/line-login-callback
+    &state=隨機字串
     &scope=profile%20openid%20email">
         <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/LINE_logo.svg" alt="使用 LINE 登入" width="150">
     </a>
 </body>
 </html>
+
--- src/java/servlets/LineLoginServlet.java
+++ src/java/servlets/LineLoginServlet.java
@@ -1,5 +1,23 @@
 package java.servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.UUID;
 
-public class LineLoginServlet {
+public class LineLoginServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 生成隨機的 state 字串
+        String state = UUID.randomUUID().toString();
+
+        // 建立 LINE 登入 URL
+        String loginUrl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code" +
+                          "&client_id=2007150725" +
+                          "&redirect_uri=http://localhost:8080/line-login-callback" +
+                          "&state=" + state + 
+                          "&scope=profile%20openid%20email";
+
+        // 將登入 URL 重定向到 LINE 登入頁面
+        response.sendRedirect(loginUrl);
+    }
     
 }
